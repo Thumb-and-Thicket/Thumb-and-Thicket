fb96e5b8b6770fb8965b1368d24aaecd0307428a
jol <imfullof137@gmail.com>
2026-09-19 07:17:43 +0100

    no more 1 block tall trees!

--- src/main/java/net/jolene/thumbandthicket/mixin/tree/TreeFeatureMixin.java
+++ src/main/java/net/jolene/thumbandthicket/mixin/tree/TreeFeatureMixin.java
@@ -2,6 +2,7 @@ package net.jolene.thumbandthicket.mixin.tree;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.jolene.thumbandthicket.block.ModBlocks;
 import net.jolene.thumbandthicket.world.gen.ModConfiguredFeatures;
@@ -9,12 +10,14 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.ModifiableWorld;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.util.FeatureContext;
+import net.minecraft.world.gen.trunk.TrunkPlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,6 +32,12 @@ public abstract class TreeFeatureMixin {
         BlockState state1 = rootBlock.getDefaultState();
 //        world.setBlockState(pos.down(), state1, Block.NOTIFY_ALL);
         world.setBlockState(pos, state, Block.NOTIFY_ALL);
+    }
+    @WrapOperation(method = "generate", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/trunk/TrunkPlacer;getHeight(Lnet/minecraft/util/math/random/Random;)I"))
+    private int thumbandthicket$increaseTreeHeight(
+            TrunkPlacer trunkPlacer, Random random, Operation<Integer> original)
+    {
+        return original.call(trunkPlacer, random) + 1;
     }
 
     @Inject(method = "generate(Lnet/minecraft/world/gen/feature/util/FeatureContext;)Z", at = @At(value = "TAIL"))
